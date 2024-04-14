c8feb558a23e59f85b67e50b9de27899ad0a4737
Thanaphat Pethdongjan <kokou102@hotmail.com>
2024-04-14 14:30:33 +0700

    fetch data from tmd api

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,28 @@
+import { Box } from "@mui/material"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
+
 function Home() {
-    return <h1>
-        Home Pages
-    </h1>
+
+    const [Todaydata, setTodaydata] = useState([])
+    const getWeatherDataToday = async () => {
+        const url = "https://data.tmd.go.th/api/WeatherToday/V2/?uid=api&ukey=api12345&format=json"
+        await axios.get(url).then((res) => {
+            setTodaydata(res.data)
+            console.log(res.data)
+        }).catch((err) => {console.log(err)});
+    }
+
+    useEffect(() => {
+            getWeatherDataToday()
+    }, [])
+
+
+    return <Box>
+        <h1>
+            Home Page
+        </h1>
+    </Box>
 }
 
 export default Home
