b6d38e1e2fb6b2a085069450db1c719a92fefabc
Thanaphat Pethdongjan <kokou102@hotmail.com>
2024-04-14 13:36:08 +0700

    redirect default page to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Menubar from './components/menubar';
 import Home from './pages/home';
 import Map from './pages/map';
@@ -12,9 +12,10 @@ function App() {
     <div>
       <Menubar />
       <Routes>
-        <Route path="/home" element={<Home />}/>
-        <Route path="/map" element={<Map />}/>
-        <Route path="/about" element={<About />}/>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/map" element={<Map />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="*" element={<Navigate to='/home'/>}/>
       </Routes>
 
     </div>
