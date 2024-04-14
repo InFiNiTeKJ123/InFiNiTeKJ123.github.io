2926e578fc0a05ad9c35e2d976e1e8c431c4e37c
Thanaphat Pethdongjan <kokou102@hotmail.com>
2024-04-14 22:51:55 +0700

    create a search bar & add to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import axios from "axios"
 import React, { useState, useEffect } from "react"
+import SearchBar from "../components/searchbar"
 
 function Home() {
 
@@ -10,19 +11,39 @@ function Home() {
         await axios.get(url).then((res) => {
             setTodaydata(res.data)
             console.log(res.data)
-        }).catch((err) => {console.log(err)});
+        }).catch((err) => { console.log(err) });
     }
 
     useEffect(() => {
-            getWeatherDataToday()
+        getWeatherDataToday()
     }, [])
 
 
-    return <Box>
-        <h1>
-            Home Page
-        </h1>
-    </Box>
+    return (
+        <Box>
+            <Box sx={{ display: 'flex', alignItems: "center", justifyContent: "center", marginTop: "2vh" }}>
+                <SearchBar />
+            </Box>
+            <Box sx={{ marginTop: "2vh" }}>
+                <Typography sx={{ fontWeight: 'bold', paddingLeft: "2vh", fontSize: "20px" }}>
+                    อากาศวันนี้
+                </Typography>
+                <Typography sx={{ paddingLeft: "2vh", fontSize: "16px" }}>
+                    14 เมษายน 2567 เวลา 19:00
+                </Typography>
+                <Typography sx={{ fontWeight: 'bold', fontSize: "20px", display: 'flex', justifyContent: 'center', paddingTop: '2vh'}}>
+                    กรุงเทพมหานคร
+                </Typography>
+            </Box>
+            <Box component='div' sx={{ display: 'flex', alignItems: "center", justifyContent:'center'}}>
+                <Box component="img" sx={{ backgroundColor:'red' ,height: "30vh", display:'flex', paddingTop: "2vh"}}
+                    src='https://cdn4.iconfinder.com/data/icons/mini-material-design-weather-icons-multi-color/32/night_thunderstorm-512.png'/>
+            </Box>
+            <Typography>
+                32c
+            </Typography>
+        </Box>
+    )
 }
 
 export default Home
